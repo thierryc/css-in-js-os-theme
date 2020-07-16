a492ec8dac01a99900bf2d0158fa0821bee5433d
Thierry Charbonnel <t@ap.cx>
2020-07-16 13:48:10 -0400

    add gh-page

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+
+
+/**
+ * 
+ * 
+ * Buttons
+ * 
+ 
+ Buttons come in many flavors and should be used for main call to actions, to submit forms, or trigger behaviors.
+
+```
+<Button>Default</Button>
+<Button variant="primary">Primary</Button>
+<Button variant="positive">Positive</Button>
+<Button variant="negative">Negative</Button>
+<Button variant="warning">Warning</Button>
+<Button variant="cancel">Warning</Button>
+
+```
+
+ */

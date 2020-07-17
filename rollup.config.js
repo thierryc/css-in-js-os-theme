e48fb61711fee407348908a52015427b67e9bf7b
Thierry Charbonnel <t@ap.cx>
2020-07-17 09:50:31 -0400

    setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named'
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named'
+    }
+  ],
+  plugins: [
+    url(),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true
+    }),
+    resolve({
+      extensions: ['.js', '.jsx']
+    }),
+    commonjs()
+  ]
+}

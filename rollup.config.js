dc926b01c8b7b12bf33967cf88782cd069277c62
Thierry Charbonnel <t@ap.cx>
2020-07-17 10:41:40 -0400

    set constant for colors

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import url from '@rollup/plugin-url'
 
 import pkg from './package.json'
 
@@ -21,10 +21,7 @@ export default {
   ],
   plugins: [
     url(),
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true
-    }),
+    babel(),
     resolve({
       extensions: ['.js', '.jsx']
     }),
---
+++ src/constants.js
@@ -0,0 +1,51 @@
+export const LightColors = {
+  Blue: { r: 0, g: 122, b: 255 },
+  Green: { r: 52, g: 199, b: 89 },
+  Indigo: { r: 88, g: 86, b: 214 },
+  Orange: { r: 255, g: 149, b: 0 },
+  Pink: { r: 255, g: 45, b: 85 },
+  Purple: { r: 175, g: 82, b: 222 },
+  Red: { r: 255, g: 59, b: 48 },
+  Teal: { r: 90, g: 200, b: 250 },
+  Yellow: { r: 255, g: 214, b: 10 },
+  gray: { r: 142, g: 142, b: 147 },
+  gray2: { r: 174, g: 174, b: 178 },
+  gray3: { r: 199, g: 199, b: 204 },
+  gray4: { r: 209, g: 209, b: 214 },
+  gray5: { r: 229, g: 229, b: 234 },
+  gray6: { r: 242, g: 242, b: 247 }
+}
+
+export const DarkColors = {
+  Blue: { r: 10, g: 132, b: 255 },
+  Green: { r: 48, g: 209, b: 88 },
+  Indigo: { r: 94, g: 92, b: 230 },
+  Orange: { r: 255, g: 159, b: 10 },
+  Pink: { r: 255, g: 55, b: 95 },
+  Purple: { r: 191, g: 90, b: 242 },
+  Red: { r: 255, g: 69, b: 58 },
+  Teal: { r: 100, g: 210, b: 255 },
+  Yellow: { r: 255, g: 214, b: 10 },
+  gray: { r: 142, g: 142, b: 147 },
+  gray2: { r: 99, g: 99, b: 102 },
+  gray3: { r: 72, g: 72, b: 74 },
+  gray4: { r: 58, g: 58, b: 60 },
+  gray5: { r: 44, g: 44, b: 46 },
+  gray6: { r: 28, g: 28, b: 30 }
+}
+
+export const LightTheme = {
+  primary: LightColors.Blue,
+  positive: LightColors.Green,
+  negative: LightColors.Red,
+  warning: LightColors.Orange,
+  neutral: LightColors.gray
+}
+
+export const DarkTheme = {
+  primary: DarkColors.Blue,
+  positive: DarkColors.Green,
+  negative: DarkColors.Red,
+  warning: DarkColors.Orange,
+  neutral: DarkColors.gray
+}
